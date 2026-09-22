d0931ad7c2218f8a823fabab9cc8bf43333bfd70
Sarah Estes <sestes2@student.cscc.edu>
2026-09-21 21:53:47 -0400

    Added notes for project plan (will delete later)

--- book_shop_inventory.py
+++ book_shop_inventory.py
@@ -1,3 +1,18 @@
 """
 
 """
+
+
+# Have current inventory in the form of a dictionary,
+#   keys will be genre, and values will be lists with 
+#   the book names
+
+# Have a series of events that add or remove 
+#   books from inventory
+
+# Have a discount system for seniors and kids using if statements
+
+# Use a while loop to print the names of the current 
+#   inventory by genre
+
+# Include user input 
