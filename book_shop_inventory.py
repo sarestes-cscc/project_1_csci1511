240e0c684c971c6a4a19dda864cd8d1fedf0f014
Sarah Estes <sestes2@student.cscc.edu>
2026-09-22 22:52:25 -0400

    Removed assignment notes from top

--- book_shop_inventory.py
+++ book_shop_inventory.py
@@ -2,23 +2,6 @@
 
 """
 
-
-# Have current inventory in the form of a dictionary,
-#   keys will be genre, and values will be lists with 
-#   the book names
-
-# Have a series of events that add or remove 
-#   books from inventory
-
-# Have a discount system for seniors and kids using if statements
-
-# Use a for loop to print the names of the current 
-#   inventory by genre
-
-# Use a while loop to update list
-
-# Include user input
-
 current_inventory = [
     "vampires of el norte", "the pirate queen",
     "the witch", "the haunting of hill house", 
