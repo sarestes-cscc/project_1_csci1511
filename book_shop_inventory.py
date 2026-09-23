4a0d47f34e03a2a353f539c7d182e0511cd0ea01
Sarah Estes <sestes2@student.cscc.edu>
2026-09-22 21:41:31 -0400

    Added ordering system for customer Customer picks book, and it adds to their list Added boolean variable to exit order system

--- book_shop_inventory.py
+++ book_shop_inventory.py
@@ -29,3 +29,21 @@ current_inventory = [
 print(f"\nWelcome to the book shop! Please take a look at "
       "our inventory:")
 
+for book in current_inventory:
+    print(book.title())
+
+customer_order = []
+ordering_book = True
+
+while ordering_book:
+    print(f"\nPlease input the books you would like to buy.")
+    book_choice = input("Enter 'quit' when you are done: ")
+    customer_order.append(book_choice)
+
+    if book_choice == 'quit':
+        ordering_book = False
+        customer_order.remove("quit")
+        print("\nThe following books have been added to your cart:")
+
+        for order in customer_order:
+            print(order.title())
